5fb793d0f7a3c2c302f4c6dffdb7bc895f952855
Thomas Muat <Thomas_Muat@hotmail.com>
2021-08-17 15:25:07 +0100

    Added login page locators

--- hudl_qa/utils/locators.py
+++ hudl_qa/utils/locators.py
@@ -1,7 +1,8 @@
-# Third Party Imports
-from selenium.webdriver.common.by import By
-
-
 class Locators:
     # --- Home Page Locators ---
-    LOGIN_BUTTON = (By.XPATH, "/html/body/div[2]/header/div[2]/ul/li[3]/a")
+    LOGIN_BUTTON = "/html/body/div[2]/header/div[2]/ul/li[3]/a"
+
+    # --- Login Page Locators ---
+    EMAIL_INPUT = "email"
+    PASSWORD_INPUT = "password"
+    SUBMIT_BUTTON = "logIn"
