1e41382add8d043d7b15587b4c1b06144de761fc
Thomas Muat <Thomas_Muat@hotmail.com>
2021-08-17 13:53:24 +0100

    Changed name of resources folder

--- hudl_qa/resources/test_data.py
+++
@@ -1,3 +0,0 @@
-class TestData:
-    CHROME_EXECUTABLE_PATH = "hudl_qa/drivers/chromedriver"
-    BASE_URL = "https://www.hudl.com/en_gb/"
---
+++ hudl_qa/utils/test_data.py
@@ -0,0 +1,5 @@
+class TestData:
+    CHROME_EXECUTABLE_PATH = (
+        "/Users/thomasmuat/Workspace/hudl_qa/hudl_qa/drivers/chromedriver"
+    )
+    BASE_URL = "https://www.hudl.com/en_gb/"
