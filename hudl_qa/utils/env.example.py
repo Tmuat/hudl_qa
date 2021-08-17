984dd7ea993603d23c15733f3ccbbbead16a9d70
Thomas Muat <Thomas_Muat@hotmail.com>
2021-08-17 14:31:30 +0100

    Moved chrome path to env

--- hudl_qa/utils/env.example.py
+++ hudl_qa/utils/env.example.py
@@ -3,3 +3,8 @@
 
 email = "your_hudl_email"
 password = "your_hudl_password"
+
+# Selinium Driver
+# ------------------------------------------------------------------------------
+
+chrome_executable_path = "/path/to/chromedriver/"
