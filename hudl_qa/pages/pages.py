837b1351832977d8654d1857634dea7d51477812
Thomas Muat <Thomas_Muat@hotmail.com>
2021-08-18 09:59:26 +0100

    Added test for empty email and password

--- hudl_qa/pages/pages.py
+++ hudl_qa/pages/pages.py
@@ -70,3 +70,6 @@ class LoginPage(BasePage):
         self.driver.find_element(By.ID, self.password_input).send_keys(self.password)
 
         self.driver.find_element(By.ID, self.submit).click()
+
+    def empty_email_and_password_login(self):
+        self.driver.find_element(By.ID, self.submit).click()
--- hudl_qa/tests/all_tests.py
+++ hudl_qa/tests/all_tests.py
@@ -85,3 +85,24 @@ class Test_HUDL_Login(Test_HUDL_Base):
         else:
             element_found = False
         self.assertTrue(element_found)
+
+    def test_004_empty_email_and_password(self):
+        """
+        Assert if the no email and password is passed, the user cannot
+        login and an error message will be shown.
+        """
+        self.loginpage = LoginPage(self.driver)
+        self.loginpage.empty_email_and_password_login()
+        time.sleep(2)
+        if (
+            len(
+                self.loginpage.driver.find_elements(
+                    By.CLASS_NAME, Locators.ERROR_LOCATOR
+                )
+            )
+            > 0
+        ):
+            element_found = True
+        else:
+            element_found = False
+        self.assertTrue(element_found)
